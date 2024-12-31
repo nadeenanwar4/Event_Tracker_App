083a60195230532bc3165826ed4d7ceb91459e13
nadeenanwar4 <nadeenanwar4@gmail.com>
2024-12-31 11:27:38 +0200

    updating notification screen

--- lib/screens/notifications.dart
+++ lib/screens/notifications.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ui_task1/core/app_colors.dart';
 import 'package:ui_task1/core/app_fonts.dart';
 import 'package:ui_task1/widgets/custom_app_bar.dart';
+import 'package:ui_task1/widgets/custom_list_tile.dart';
 import 'package:ui_task1/widgets/customized_text.dart';
 
 void main() {
@@ -16,6 +17,7 @@ class NotificationScreen extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: AppColors.colorF8F8F8,
         body: Column(
           children: [
             CustomizedAppBar(
@@ -29,41 +31,62 @@ class NotificationScreen extends StatelessWidget {
             Expanded(
               child: ListView.builder(
                 itemCount: 6,
-                padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                padding: EdgeInsets.all(16),
                 itemBuilder: (context, index) {
                   return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 4.0),
-                    child: Directionality(
-                      textDirection: TextDirection.rtl,
-                      child: ListTile(
-                        //minVerticalPadding: 20,
-                        isThreeLine: true,
-                        title: Padding(
-                          padding: const EdgeInsets.only(bottom: 4.0),
-                          child: CustomizedText(
-                              txt: "فعالية جديدة !",
-                              fontSize: 16,
-                              txtColor: AppColors.color3f4857,
-                              fontFamily: AppFonts.primaryFontFamily,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        subtitle: CustomizedText(
-                            txt:
-                                "تم إضافة فعالية جديدة انت مهتم بها من قبل شركة سفاري الق نظرة عليها الآن",
-                            fontSize: 14,
-                            txtColor: AppColors.color797F8A,
-                            fontFamily: AppFonts.primaryFontFamily,
-                            fontWeight: FontWeight.normal),
-                        tileColor: AppColors.whiteColor,
-                        shape: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                          borderSide: BorderSide(
-                            color: AppColors.whiteColor,
-                          ),
-                        ),
-                      ),
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: CustomizedListTile(
+                      title: "فعالية جديدة !",
+                      titleTxtStyle: TextStyle(
+                          fontSize: 16,
+                          color: AppColors.color3f4857,
+                          fontFamily: AppFonts.primaryFontFamily,
+                          fontWeight: FontWeight.bold),
+                      subTitle:
+                          "تم إضافة فعالية جديدة انت مهتم بها من قبل شركة سفاري الق نظرة عليها الآن",
+                      subTxtStyle: TextStyle(
+                          fontSize: 14,
+                          color: AppColors.color797F8A,
+                          fontFamily: AppFonts.primaryFontFamily,
+                          fontWeight: FontWeight.normal),
+                      is3Lines: true,
+                      radius: 8,
                     ),
                   );
+
+                  // Padding(
+                  //   padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  //   child: Directionality(
+                  //     textDirection: TextDirection.rtl,
+                  //     child: ListTile(
+                  //       //minVerticalPadding: 20,
+                  //       isThreeLine: true,
+                  //       title: Padding(
+                  //         padding: const EdgeInsets.only(bottom: 4.0),
+                  //         child: CustomizedText(
+                  //             txt: "فعالية جديدة !",
+                  //             fontSize: 16,
+                  //             txtColor: AppColors.color3f4857,
+                  //             fontFamily: AppFonts.primaryFontFamily,
+                  //             fontWeight: FontWeight.bold),
+                  //       ),
+                  //       subtitle: CustomizedText(
+                  //           txt:
+                  //               "تم إضافة فعالية جديدة انت مهتم بها من قبل شركة سفاري الق نظرة عليها الآن",
+                  //           fontSize: 14,
+                  //           txtColor: AppColors.color797F8A,
+                  //           fontFamily: AppFonts.primaryFontFamily,
+                  //           fontWeight: FontWeight.normal),
+                  //       tileColor: AppColors.whiteColor,
+                  //       shape: OutlineInputBorder(
+                  //         borderRadius: BorderRadius.circular(8),
+                  //         borderSide: BorderSide(
+                  //           color: AppColors.whiteColor,
+                  //         ),
+                  //       ),
+                  //     ),
+                  //   ),
+                  // );
                 },
               ),
             ),
